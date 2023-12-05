facad4db48f9418ff39f1742172ea52c53ea244c
leandro-cajal <leo_origin@hotmail.com>
2023-12-05 19:29:16 -0300

    Correccion del js

--- js/main.js
+++ js/main.js
@@ -62,25 +62,27 @@
 // }while(usuario === "si")
 
 
-
- let prestamo = 0, numCuotas, interes, montoInicial, montoFinal, valorDeCuota, respuestaUsuario = "no";
+/*
+ let montoInicial, respuestaUsuario = no;
 
  function simuladorCuotas (montoInicial, numCuotas){
+        let valorDeCuota;
+        let montoFinal;
         switch(numCuotas){
                 case 3: 
-                montoFinal = montoInicial + montoInicial * 20/100;
+                montoFinal = montoInicial * 1.2;
                 valorDeCuota = montoFinal/3;
                 alert(`Por el prestamo de $${montoInicial} el monto a abonar con interes seria de $${montoFinal}, y el valor de cada cuota seria de $${valorDeCuota}`);
                 break;
 
                 case 6: 
-                montoFinal = montoInicial + montoInicial * 30/100;
+                montoFinal = montoInicial * 1.4;
                 valorDeCuota = montoFinal/6;
                 alert(`Por el prestamo de $${montoInicial} el monto a abonar con interes seria de $${montoFinal}, y el valor de cada cuota seria de :$${valorDeCuota}`);
                 break;
 
                 case 12: 
-                montoFinal = montoInicial + montoInicial * 50/100;
+                montoFinal = montoInicial * 1.6;
                 valorDeCuota = montoFinal/12;
                 alert(`Por el prestamo de $${montoInicial} el monto a abonar con interes seria de $${montoFinal}, y el valor de cada cuota seria de $${valorDeCuota}`);
                 break;
@@ -91,14 +93,75 @@
  }
 
  do{
-     prestamo = parseInt(prompt("Ingrese el valor del monto a prestar"));
-     numCuotas = parseInt(prompt("Ingrese numero de cuotas, 3, 6 o 12"));
-     simuladorCuotas(prestamo, numCuotas);
-     respuestaUsuario = prompt("Desea realizar otra consulta? responda si/no").toLowerCase();
-     if (respuestaUsuario === "no"){
-        alert ("Gracias por consultar con nosotros!")
+     let prestamo = 0
+     prestamo = parseInt(prompt("Ingrese el valor del prestamo"));
+     if( prestamo === NaN || prestamo === "" || prestamo < 1000){
+        prestamo = parseInt(prompt("Ha ingresado un monto invalido, ingrese un monto valido para permanecer en la pagina"));
+        if (prestamo === NaN || prestamo === "" || prestamo < 1000){
+            alert ("Gracias por consultar con nosotros!")
+            break; 
+        }  
      }
- }while(respuestaUsuario === "si")
+     let numCuotas = parseInt(prompt("Ingrese numero de cuotas, 3, 6 o 12"));
+     simuladorCuotas(prestamo, numCuotas);
+     respuestaUsuario = prompt("Desea realizar otra consulta? Responda si/no").toLowerCase();
+    while (respuestaUsuario !== "si" && respuestaUsuario !== "no") {
+        respuestaUsuario = prompt("Ingrese una respuesta válida (si/no)");
+    }
+    
+    if (respuestaUsuario === "no") {
+        alert("Gracias por consultar con nosotros!");
+    }
+} while (respuestaUsuario === "si");
+*/
 
+let montoInicial, respuestaUsuario;
 
+function simuladorCuotas(montoInicial, numCuotas) {
+    let valorDeCuota;
+    let montoFinal;
+    switch (numCuotas) {
+        case 3:
+            montoFinal = montoInicial * 1.2;
+            valorDeCuota = montoFinal / 3;
+            alert(`Por el préstamo de $${montoInicial} el monto a abonar con interés sería de $${montoFinal}, y el valor de cada cuota sería de $${valorDeCuota}`);
+            break;
+
+        case 6:
+            montoFinal = montoInicial * 1.4;
+            valorDeCuota = montoFinal / 6;
+            alert(`Por el préstamo de $${montoInicial} el monto a abonar con interés sería de $${montoFinal}, y el valor de cada cuota sería de $${valorDeCuota}`);
+            break;
+
+        case 12:
+            montoFinal = montoInicial * 1.6;
+            valorDeCuota = montoFinal / 12;
+            alert(`Por el préstamo de $${montoInicial} el monto a abonar con interés sería de $${montoFinal}, y el valor de cada cuota sería de $${valorDeCuota}`);
+            break;
+
+        default:
+            alert("El número de cuotas indicadas no son válidas");
+    }
+}
+
+do {
+    respuestaUsuario = prompt("Desea realizar una consulta? Responda si/no").toLowerCase();
+    while (respuestaUsuario !== "si" && respuestaUsuario !== "no") {
+        respuestaUsuario = prompt("Su respuesta no es válida, desea realizar una consulta? Responda si/no").toLowerCase();
+    }
+
+    if (respuestaUsuario !== "si") {
+        alert("Gracias por consultar con nosotros!");
+        break;
+    }
+
+    let prestamo = parseInt(prompt("Ingrese el valor del préstamo"));
+
+    if (isNaN(prestamo) || prestamo < 1000) {
+        alert("Ha ingresado un monto inválido.");
+    }else{
+        let numCuotas = parseInt(prompt("Ingrese número de cuotas, 3, 6 o 12"));
+        simuladorCuotas(prestamo, numCuotas);
+    }
+} while (respuestaUsuario === "si");
 
