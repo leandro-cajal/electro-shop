b7764e75f55ebab9fc836c3c0222ac8c5737cc60
leandro-cajal <leo_origin@hotmail.com>
2023-12-13 22:34:01 -0300

    menu hamburguesa en mobile terminado

--- js/main.js
+++ js/main.js
@@ -1,61 +1,8 @@
-/*let montoInicial, respuestaUsuario;
-
-function simuladorCuotas(montoInicial, numCuotas) {
-    let valorDeCuota;
-    let montoFinal;
-    switch (numCuotas) {
-        case 3:
-            montoFinal = montoInicial * 1.2;
-            valorDeCuota = montoFinal / 3;
-            alert(`Por el préstamo de $${montoInicial} el monto a abonar con interés sería de $${montoFinal}, y el valor de cada cuota sería de $${valorDeCuota}`);
-            break;
-
-        case 6:
-            montoFinal = montoInicial * 1.4;
-            valorDeCuota = montoFinal / 6;
-            alert(`Por el préstamo de $${montoInicial} el monto a abonar con interés sería de $${montoFinal}, y el valor de cada cuota sería de $${valorDeCuota}`);
-            break;
-
-        case 12:
-            montoFinal = montoInicial * 1.6;
-            valorDeCuota = montoFinal / 12;
-            alert(`Por el préstamo de $${montoInicial} el monto a abonar con interés sería de $${montoFinal}, y el valor de cada cuota sería de $${valorDeCuota}`);
-            break;
-
-        default:
-            alert("El número de cuotas indicadas no son válidas");
-    }
-}
-
-do {
-    respuestaUsuario = prompt("Desea realizar una consulta? Responda si/no").toLowerCase();
-    while (respuestaUsuario !== "si" && respuestaUsuario !== "no"){
-        respuestaUsuario = prompt("Su respuesta no es válida, desea realizar una consulta? Responda si/no").toLowerCase();
-    }
-
-    if (respuestaUsuario !== "si") {
-        alert("Gracias por consultar con nosotros!");
-        break;
-    }
-
-    let prestamo = parseInt(prompt("Ingrese el valor del préstamo"));
-
-    if (isNaN(prestamo) || prestamo < 1000) {
-        alert("Ha ingresado un monto inválido.");
-    }else{
-        let numCuotas = parseInt(prompt("Ingrese número de cuotas, 3, 6 o 12"));
-        simuladorCuotas(prestamo, numCuotas);
-    }
-
-} while (respuestaUsuario === "si");*/
-
-
-'use strict';
-
 function setupCarousel(carouselSelector, indicatorSelector) {
     const carousel = document.querySelector(carouselSelector);
     const carouselIndicators = document.querySelectorAll(indicatorSelector);
     let currentIndex = 0;
+    let intervalId;
 
     const moveCarousel = (index) => {
         let operation = index * -100;
@@ -63,11 +10,11 @@ function setupCarousel(carouselSelector, indicatorSelector) {
 
         // restablecer todos los indicadores a su estado original
         carouselIndicators.forEach((indicator) => {
-            indicator.classList.remove('bg-stone-700');
+            indicator.style.backgroundColor = '';
         });
 
         // aplicar el fondo al indicador correspondiente
-        carouselIndicators[index].classList.add('bg-stone-700');
+        carouselIndicators[index].style.backgroundColor = 'rgb(68, 64, 60)';
     };
 
     carouselIndicators.forEach((indicator, i) => {
@@ -76,40 +23,33 @@ function setupCarousel(carouselSelector, indicatorSelector) {
             moveCarousel(currentIndex);
         });
     });
-
-    // inicializar swiper.js solo si el ancho de la ventana es menor o igual a 768px
-    if (window.innerWidth <= 768) {
-        new Swiper(carousel, {
-            slidesPerView: 1,
-            spaceBetween: 10,
-            loop: true,
-            allowTouchMove: true,
-            autoplay: {
-                delay: 8000,
-                disableOnInteraction: false,
-            },
-        });
-    }
-
-    // funcion para mover automaticamente el carrusel cada 8 segundos
-    const autoMoveCarousel = () => {
-        currentIndex = (currentIndex + 1) % carouselIndicators.length;
-        moveCarousel(currentIndex);
-    };
-
-    // configurar intervalo para el movimiento automatico cada 8 segundos
-    let intervalId = setInterval(autoMoveCarousel, 8000);
-
-    // detener el intervalo cuando el mouse esta sobre el carrusel
-    carousel.addEventListener('mouseenter', () => {
-        clearInterval(intervalId);
-    });
-
-    // reanudar el intervalo cuando el mouse sale del carrusel
-    carousel.addEventListener('mouseleave', () => {
-        intervalId = setInterval(autoMoveCarousel, 8000);
-    });
 }
 
 setupCarousel('.carousel', '.carousel-indicator');
 setupCarousel('.carousel-mobile', '.mobile-carousel-indicator');
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const abrir = document.querySelector("#open-menu");
+    const cerrar = document.querySelector("#close-menu");
+    const menu = document.querySelector("#menu");
+    const nav = document.querySelector("#nav");
+
+    abrir.addEventListener("click", () => {
+        nav.style.display = 'block';
+        setTimeout(() => {
+            menu.style.transition = 'transform 0.3s';
+            menu.style.transform = 'translateX(0)';
+        }, 50); // 50 milisegundos de retraso antes de aplicar la transición
+    });
+
+    cerrar.addEventListener("click", () => {
+        menu.style.transition = 'transform 0.3s';
+        menu.style.transform = 'translateX(-100%)';
+
+        // Espera a que termine la transición antes de ocultar el elemento
+        setTimeout(() => {
+            nav.style.display = 'none';
+        }, 300); // 300 milisegundos, ajusta según la duración de tu transición
+    });
+});
