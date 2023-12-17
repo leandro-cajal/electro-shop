1194e2cbc22b316bf16a822a6019c4e5ee974cd4
leandro-cajal <leo_origin@hotmail.com>
2023-12-16 23:56:21 -0300

    comenzando segunda section de home

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ function setupCarousel(carouselSelector, indicatorSelector) {
     const carousel = document.querySelector(carouselSelector);
     const carouselIndicators = document.querySelectorAll(indicatorSelector);
     let currentIndex = 0;
+    let intervalId;
 
     const moveCarousel = (index) => {
         let operation = index * -100;
@@ -23,16 +24,28 @@ function setupCarousel(carouselSelector, indicatorSelector) {
 
     // Iniciar el intervalo para cambiar automáticamente las diapositivas cada 8 segundos
     const intervalDuration = 8000; // 8 segundos
-    const intervalId = setInterval(nextSlide, intervalDuration);
+    intervalId = setInterval(nextSlide, intervalDuration);
 
-    // Detener el intervalo cuando el mouse está sobre el carrusel (opcional)
+    // Detener el intervalo cuando el mouse está sobre el carrusel
     carousel.addEventListener('mouseover', () => {
         clearInterval(intervalId);
     });
 
-    // Reanudar el intervalo cuando el mouse sale del carrusel (opcional)
+    // Reanudar el intervalo cuando el mouse sale del carrusel
     carousel.addEventListener('mouseout', () => {
         intervalId = setInterval(nextSlide, intervalDuration);
+    });
+
+    // Agregar eventos de clic a los indicadores para cambiar manualmente las diapositivas
+    carouselIndicators.forEach((indicator, i) => {
+        indicator.addEventListener('click', () => {
+            currentIndex = i;
+            moveCarousel(currentIndex);
+
+            // Restablecer el intervalo después de hacer clic en un indicador
+            clearInterval(intervalId);
+            intervalId = setInterval(nextSlide, intervalDuration);
+        });
     });
 }
 
@@ -50,12 +63,19 @@ document.addEventListener('DOMContentLoaded', () => {
     const menu = document.querySelector("#menu");
     const nav = document.querySelector("#nav");
 
+    let intervalId;
+
     abrir.addEventListener("click", () => {
         nav.style.display = 'block';
+
+        // Espera a que termine la transición antes de aplicar la transformación del menú
         setTimeout(() => {
             menu.style.transition = 'transform 0.3s';
             menu.style.transform = 'translateX(0)';
         }, 50); // 50 milisegundos de retraso antes de aplicar la transición
+
+        // Detener el intervalo del carrusel cuando se abre el menú
+        clearInterval(intervalId);
     });
 
     cerrar.addEventListener("click", () => {
@@ -66,5 +86,33 @@ document.addEventListener('DOMContentLoaded', () => {
         setTimeout(() => {
             nav.style.display = 'none';
         }, 300); // 300 milisegundos, ajusta según la duración de tu transición
+
+        // Reanudar el intervalo del carrusel cuando se cierra el menú
+        intervalId = setInterval(nextSlide, intervalDuration);
+    });
+
+    // Agregar evento de clic al documento para cerrar el menú cuando se hace clic fuera
+    document.addEventListener('click', (event) => {
+        const isClickInsideMenu = menu.contains(event.target);
+        const isClickOnOpenButton = abrir.contains(event.target);
+        
+        // Cierra el menú si se hace clic fuera y el menú está abierto
+        if (!isClickInsideMenu && !isClickOnOpenButton) {
+            menu.style.transition = 'transform 0.3s';
+            menu.style.transform = 'translateX(-100%)';
+
+            // Espera a que termine la transición antes de ocultar el elemento
+            setTimeout(() => {
+                nav.style.display = 'none';
+            }, 300); // 300 milisegundos, ajusta según la duración de tu transición
+
+            // Reanudar el intervalo del carrusel cuando se cierra el menú
+            intervalId = setInterval(nextSlide, intervalDuration);
+        }
+    });
+
+    // Evitar que el clic en el menú propague hasta el documento
+    menu.addEventListener('click', (event) => {
+        event.stopPropagation();
     });
 });
