2e8ae01041977383a17dbcaad6922ff7331c17f7
leandro-cajal <leo_origin@hotmail.com>
2024-01-04 00:44:56 -0300

    banner finalizado y modificando carrusel de items y navbar desplegable en desktop

--- js/main.js
+++ js/main.js
@@ -2,24 +2,35 @@ function setupCarousel(carouselSelector, indicatorSelector) {
     const carousel = document.querySelector(carouselSelector);
     const carouselIndicators = document.querySelectorAll(indicatorSelector);
     let currentIndex = 0;
+    let startX = 0;
+    let isDragging = false;
     let intervalId;
 
-    const moveCarousel = (index) => {
-        let operation = index * -100;
+    const moveCarousel = () => {
+        const operation = currentIndex * -100;
         carousel.style.transform = `translateX(${operation}%)`;
+        updateIndicators();
+    };
 
-        // restablecer todos los indicadores a su estado original
-        carouselIndicators.forEach((indicator) => {
-            indicator.style.backgroundColor = '';
+    const updateIndicators = () => {
+        carouselIndicators.forEach((indicator, i) => {
+            indicator.style.backgroundColor = i === currentIndex ? 'rgb(68, 64, 60)' : '';
         });
+    };
 
-        // aplicar el fondo al indicador correspondiente
-        carouselIndicators[index].style.backgroundColor = 'rgb(68, 64, 60)';
+    const handleSlideChange = (direction) => {
+        currentIndex = (currentIndex + direction + carouselIndicators.length) % carouselIndicators.length;
+        moveCarousel();
+        resetInterval();
     };
 
     const nextSlide = () => {
-        currentIndex = (currentIndex + 1) % carouselIndicators.length;
-        moveCarousel(currentIndex);
+        handleSlideChange(1);
+    };
+
+    const resetInterval = () => {
+        clearInterval(intervalId);
+        intervalId = setInterval(nextSlide, intervalDuration);
     };
 
     // Iniciar el intervalo para cambiar automáticamente las diapositivas cada 8 segundos
@@ -33,20 +44,73 @@ function setupCarousel(carouselSelector, indicatorSelector) {
 
     // Reanudar el intervalo cuando el mouse sale del carrusel
     carousel.addEventListener('mouseout', () => {
-        intervalId = setInterval(nextSlide, intervalDuration);
+        resetInterval();
+    });
+
+    // Solo permitir eventos de arrastre en pantallas más pequeñas
+    if (window.innerWidth <= 1023) {
+        carousel.addEventListener('mousedown', (e) => {
+            startX = e.clientX;
+            isDragging = true;
+        });
+
+        carousel.addEventListener('mouseup', (e) => {
+            if (isDragging) {
+                const deltaX = e.clientX - startX;
+                if (Math.abs(deltaX) > 50) {
+                    // Deslizamiento suficientemente grande, cambiar de diapositiva
+                    handleSlideChange(deltaX > 0 ? -1 : 1);
+                }
+                isDragging = false;
+            }
+        });
+
+        carousel.addEventListener('mousemove', (e) => {
+            if (isDragging) {
+                const deltaX = e.clientX - startX;
+                const operation = (currentIndex * -100) + (deltaX / window.innerWidth) * 100;
+                carousel.style.transform = `translateX(${operation}%)`;
+            }
+        });
+
+        carousel.addEventListener('mouseleave', () => {
+            if (isDragging) {
+                isDragging = false;
+                moveCarousel();
+            }
+        });
+    }
+
+    carousel.addEventListener('touchstart', (e) => {
+        startX = e.touches[0].clientX;
+    });
+
+    carousel.addEventListener('touchmove', (e) => {
+        const deltaX = e.touches[0].clientX - startX;
+        const operation = (currentIndex * -100) + (deltaX / window.innerWidth) * 100;
+        carousel.style.transform = `translateX(${operation}%)`;
+    });
+
+    carousel.addEventListener('touchend', (e) => {
+        const deltaX = e.changedTouches[0].clientX - startX;
+
+        if (Math.abs(deltaX) > 50) {
+            // Deslizamiento suficientemente grande, cambiar de diapositiva
+            handleSlideChange(deltaX > 0 ? -1 : 1);
+        }
     });
 
     // Agregar eventos de clic a los indicadores para cambiar manualmente las diapositivas
     carouselIndicators.forEach((indicator, i) => {
         indicator.addEventListener('click', () => {
             currentIndex = i;
-            moveCarousel(currentIndex);
-
-            // Restablecer el intervalo después de hacer clic en un indicador
-            clearInterval(intervalId);
-            intervalId = setInterval(nextSlide, intervalDuration);
+            moveCarousel();
+            resetInterval();
         });
     });
+
+    // Iniciar carrusel
+    moveCarousel();
 }
 
 // Iniciar carruseles
@@ -54,60 +118,71 @@ setupCarousel('.carousel', '.carousel-indicator');
 setupCarousel('.carousel-mobile', '.mobile-carousel-indicator');
 
 
-//MENU HAMBURGUESA
 
+//MENU HAMBURGUESA
 
 document.addEventListener('DOMContentLoaded', () => {
     const abrir = document.querySelector("#open-menu");
     const cerrar = document.querySelector("#close-menu");
     const menu = document.querySelector("#menu");
     const nav = document.querySelector("#nav");
-
+    const abrir2 = document.querySelector("#open-menu-full");
+    const menuContainer = document.querySelector("#menu-container")
     let intervalId;
 
-    abrir.addEventListener("click", () => {
+    const abrirMenu = () => {
         nav.style.display = 'block';
-
+        if (window.innerWidth < 1024){
         // Espera a que termine la transición antes de aplicar la transformación del menú
         setTimeout(() => {
             menu.style.transition = 'transform 0.3s';
             menu.style.transform = 'translateX(0)';
         }, 50); // 50 milisegundos de retraso antes de aplicar la transición
-
+        }
         // Detener el intervalo del carrusel cuando se abre el menú
         clearInterval(intervalId);
-    });
+    };
 
-    cerrar.addEventListener("click", () => {
-        menu.style.transition = 'transform 0.3s';
-        menu.style.transform = 'translateX(-100%)';
-
+    const cerrarMenu = () => {
+        if (window.innerWidth < 1024) {
+            menu.style.transition = 'transform 0.3s';
+            menu.style.transform = 'translateX(-100%)';
+        }
+    
         // Espera a que termine la transición antes de ocultar el elemento
         setTimeout(() => {
             nav.style.display = 'none';
         }, 300); // 300 milisegundos, ajusta según la duración de tu transición
-
+    
         // Reanudar el intervalo del carrusel cuando se cierra el menú
         intervalId = setInterval(nextSlide, intervalDuration);
+    };
+    
+    // ...
+    
+    window.addEventListener('resize', () => {
+        // Al cambiar el tamaño de la ventana, verifica si estás en mobile o desktop
+        if (window.innerWidth >= 1024) {
+            // Si es desktop, elimina los estilos modificados
+            menu.style.transition = '';
+            menu.style.transform = '';
+        } else {
+            // Si es mobile y el menú está abierto, establece translateX en 0
+            if (nav.style.display === 'block') {
+                menu.style.transition = 'transform 0.3s';
+                menu.style.transform = 'translateX(0)';
+            }
+        }
     });
-
+ 
     // Agregar evento de clic al documento para cerrar el menú cuando se hace clic fuera
     document.addEventListener('click', (event) => {
-        const isClickInsideMenu = menu.contains(event.target);
+        const isClickInsideMenu = menuContainer.contains(event.target);
         const isClickOnOpenButton = abrir.contains(event.target);
-        
+
         // Cierra el menú si se hace clic fuera y el menú está abierto
         if (!isClickInsideMenu && !isClickOnOpenButton) {
-            menu.style.transition = 'transform 0.3s';
-            menu.style.transform = 'translateX(-100%)';
-
-            // Espera a que termine la transición antes de ocultar el elemento
-            setTimeout(() => {
-                nav.style.display = 'none';
-            }, 300); // 300 milisegundos, ajusta según la duración de tu transición
-
-            // Reanudar el intervalo del carrusel cuando se cierra el menú
-            intervalId = setInterval(nextSlide, intervalDuration);
+            cerrarMenu();
         }
     });
 
@@ -115,4 +190,47 @@ document.addEventListener('DOMContentLoaded', () => {
     menu.addEventListener('click', (event) => {
         event.stopPropagation();
     });
+
+    cerrar.addEventListener("click", cerrarMenu);
+    abrir.addEventListener("click", abrirMenu);
+    abrir2.addEventListener("click", (event) => {
+        // Evitar que el clic en abrir2 se propague al documento
+        event.stopPropagation();
+        abrirMenu();
+    });
 });
+
+// CARRUSEL DE CATEGORIAS HOME
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Selecciona elementos del DOM
+    const prevButton = document.getElementById("carousel-btn-prev");
+    const nextButton = document.getElementById("carousel-btn-next");
+    const carouselContainer = document.getElementById("carousel-cat-container");
+    const carouselList = document.getElementById("carousel-categories");
+
+    // Calcula la anchura de un elemento y el margen
+    const itemWidth = carouselList.firstElementChild.clientWidth + 16; // Incluye el margen de 4px de cada lado
+    const itemsToShow = 6; // Número de elementos a mostrar en el carrusel
+    const scrollIncrement = itemWidth * 2; // Desplazamiento en píxeles al hacer clic en los botones
+
+    // Configura el evento click para los botones previo y siguiente
+    prevButton.addEventListener("click", function () {
+      scrollCarousel("prev");
+    });
+
+    nextButton.addEventListener("click", function () {
+      scrollCarousel("next");
+    });
+
+    // Función para desplazar el carrusel
+    function scrollCarousel(direction) {
+      const currentScroll = carouselContainer.scrollLeft;
+
+      if (direction === "prev") {
+        carouselContainer.scrollLeft = Math.max(0, currentScroll - scrollIncrement);
+      } else {
+        carouselContainer.scrollLeft = Math.min(carouselList.scrollWidth - carouselContainer.clientWidth, currentScroll + scrollIncrement);
+      }
+    }
+  });
